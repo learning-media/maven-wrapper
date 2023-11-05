532f56e2bff420014af4ccedf37d0cb5d15b6e07
Umesh Yadav <umeshyadav@Umeshs-MacBook-Pro.local>
2023-11-06 00:15:27 +0530

    initial commit

---
+++ src/main/java/com/learningmedia/service/mavenwrapper/MavenWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.learningmedia.service.mavenwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningmedia/service/mavenwrapper/MavenWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningmedia.service.mavenwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
